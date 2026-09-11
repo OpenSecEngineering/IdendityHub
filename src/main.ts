0a1f02a2b4f3f7baf53dacfeef53d0cd9251df26
ZouFitahiana <fitahianaherizo10@gmail.com>
2026-09-11 22:11:04 +0300

    feat swagger: add and setup swagger: :

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Identity Hub API')
+    .setDescription(
+      'Identity and Access Management API for OpenSecEngineering',
+    )
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(
+    app,
+    swaggerConfig,
+  );
+
+  SwaggerModule.setup('docs', app, swaggerDocument);
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
